50a2e470425d945c260fd0adbf83cc63791d42f6
Ning Li <lining04111223@gmail.com>
2022-11-11 09:46:43 -0800

    Panel Component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,61 @@
 import React, { Component } from "react";
+import Loading from "./Loading";
+import Panel from "./Panel";
 
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
-    return <main className={dashboardClasses} />;
+class Dashboard extends Component {
+
+  state = {
+    loading: false
+  };
+
+  render() {
+  const dashboardClasses = classnames("dashboard");
+  const newdata=data.map((item) =>(
+      <Panel
+      key={item.id}
+      id={item.id} 
+      label={item.label} 
+      value={item.value}
+    />
+  ));
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    return (
+    <main className={dashboardClasses}>
+    
+     {newdata}
+    
+    </main>
+    
+    );
   }
 }
 
